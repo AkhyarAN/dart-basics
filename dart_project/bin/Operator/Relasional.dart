c2bb29b7e049a1952e5edf8650fb83a8862d458d
AkhyarAN <akhyarnurullah@gmail.com>
2025-03-09 21:32:33 +0700

    Operator

---
+++ dart_project/bin/Operator/Relasional.dart
@@ -0,0 +1,15 @@
+void main() {
+  int nilai = 97;
+  int batasnilai = 72;
+  if (nilai <= batasnilai)
+    print('tidak lulus');
+  else
+    print('lulus');
+
+  String nama = 'Akhyar';
+  String nama2 = 'Nurullah';
+  if (nama != nama2)
+    print("tidak sama");
+  else
+    print('sama');
+}
